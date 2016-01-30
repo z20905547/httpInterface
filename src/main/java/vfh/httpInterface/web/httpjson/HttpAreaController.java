5fabe3a30d865c53f7110a9e838615a60dc61efa
harry <359705093@qq.com>
2016-01-31 03:12:10 +0800

    ----add city select js file and function.had used in add.html
    Signed-off-by: harry <359705093@qq.com>

--- src/main/java/vfh/httpInterface/web/httpjson/HttpAreaController.java
+++ src/main/java/vfh/httpInterface/web/httpjson/HttpAreaController.java
@@ -1,11 +1,38 @@
 package vfh.httpInterface.web.httpjson;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import vfh.httpInterface.commons.RequestParamUtil;
+import vfh.httpInterface.service.area.AreaService;
 
 @Controller
 @RequestMapping("/jsondata/area")
 @SuppressWarnings(value={"rawtypes","unchecked"})
 public class HttpAreaController {
-
+	@Autowired
+    private AreaService areaService;
+	@RequestMapping("/getSubAreaList")
+	@ResponseBody
+	public void getSubAreaList(HttpServletRequest request,HttpServletResponse response){
+		Map res = new HashMap();
+		Map params = RequestParamUtil.requestToMap(request);
+		Map m =  areaService.getSubList(params);
+		if("000000".equals(m.get("returnCode"))){
+			res.put("data", m.get("data"));
+			res.put("statusCode","0000");
+		}else{
+			res.put("statusCode","1111");
+		}
+		res.put("returnMsg",m.get("returnMsg"));
+		RequestParamUtil.responseWriter(request, response, res);
+	}
 }
---
+++ src/main/webapp/resource/script/area.js
@@ -0,0 +1,34 @@
+
+function getSubList(pid,id,obj){
+	obj.html("");
+	obj.val("");
+	obj.append("<option>--请选择--</option>");
+	if(!pid){
+		return;
+	}
+	var url="/httpInterface/jsondata/area/getSubAreaList";
+	var params={parentId:pid};
+	$.ajax({
+		type: 'get',
+		url:url,
+		async: false,
+		dataType: 'jsonp',
+		data:params,
+		success:function(data){
+			if(data.statusCode=="0000"){
+				var sublist=data.data;
+				for(var i=0;i<sublist.length;i++){
+					if(id&&sublist[i].area_id==id){
+						obj.append("<option value='"+sublist[i].area_id+"' selected>"+sublist[i].area_name+"</option>");
+					}else{
+						obj.append("<option value='"+sublist[i].area_id+"'>"+sublist[i].area_name+"</option>");
+					}
+				}
+			}
+			
+		},
+		error:function(data){
+			alert("网络错误");
+		}
+	});
+}
